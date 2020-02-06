a6a189add14079ae421f3cee74547d0966f4d7d4
kokiebisu <a01056715@gmail.com>
2020-02-05 21:25:50 -0800

    added skeleton to featured card

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "be2f5bc2b8ee808ad266";
+/******/ 	var hotCurrentHash = "b4b317575743ed132347";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
