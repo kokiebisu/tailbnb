88f607ed147524f7be88d6ce99c1aabccbdb2114
kokiebisu <a01056715@gmail.com>
2020-02-07 09:06:42 -0800

    fixed #39

--- __tests__/footer.test.js
+++ __tests__/footer.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+// Component
 import Footer from '../src/components/Footer';
 
 describe('Footer', () => {
--- __tests__/pluscard.test.js
+++ __tests__/pluscard.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+// Component
 import PlusCard from '../src/components/PlusCard';
 
 describe('PlusCard', () => {
