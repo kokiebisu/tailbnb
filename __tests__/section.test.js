12e589e8fd5be0bcce4e52d88e10d7c186d6fa00
kokiebisu <a01056715@gmail.com>
2020-02-07 20:20:30 -0800

    fixed #51

---
+++ __tests__/section.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+// Component
+import Section from '../src/components/Section';
+
+describe('Section', () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<Section />);
+    expect(component).toMatchSnapshot();
+  });
+});
