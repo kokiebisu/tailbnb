f18f0c4ddae2765cf59a4eb548e1d15a36823426
kokiebisu <a01056715@gmail.com>
2020-02-08 16:55:37 -0800

    fixed #82

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "db5bf7bcad19d8050049";
+/******/ 	var hotCurrentHash = "039e7b4268e4e56cff08";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/TopRated.js
+++ src/components/TopRated.js
@@ -11,45 +11,50 @@ import experience3 from '../../public/img/experience-3.jpg';
 import experience4 from '../../public/img/experience-4.jpg';
 
 export default () => {
+  const toprateds = [
+    {
+      img: experience1,
+      location: 'Italy',
+      title: 'Luca & Lorenzo, cooking in Chianti hills',
+      price: 172,
+      reviews: 1765
+    },
+    {
+      img: experience2,
+      location: 'Italy',
+      title: 'Truffle experience Rome',
+      price: 148,
+      reviews: 621
+    },
+    {
+      img: experience3,
+      location: 'Japan',
+      title: 'Feeling Samurai Soul',
+      price: 119,
+      reviews: 1047
+    },
+    {
+      img: experience4,
+      location: 'Italy',
+      title: 'Lisbon best flavors',
+      price: 90,
+      reviews: 3733
+    }
+  ];
   return (
     <>
       <div className='flex items-start justify-start flex-wrap w-full'>
-        <div className='md:w-1/4 sm:w-1/3 w-1/2 pb-5'>
-          <TopRatedCard
-            img={experience1}
-            location='Italy'
-            title='Luca & Lorenzo, cooking in Chianti hills'
-            price='172'
-            reviews='1765'
-          />
-        </div>
-        <div className='md:w-1/4 sm:w-1/3 w-1/2 pb-5'>
-          <TopRatedCard
-            img={experience2}
-            location='Italy'
-            title='Truffle experience Rome'
-            price='148'
-            reviews='621'
-          />
-        </div>
-        <div className='md:w-1/4 sm:w-1/3 w-1/2 pb-5'>
-          <TopRatedCard
-            img={experience3}
-            location='Japan'
-            title='Feeling Samurai Soul'
-            price='119'
-            reviews='1047'
-          />
-        </div>
-        <div className='md:w-1/4 sm:w-1/3 w-1/2 pb-5'>
-          <TopRatedCard
-            img={experience4}
-            location='Italy'
-            title='Lisbon best flavors'
-            price='90'
-            reviews='3733'
-          />
-        </div>
+        {toprateds.map(({ img, location, title, price, reviews }) => {
+          <div className='md:w-1/4 sm:w-1/3 w-1/2 pb-5'>
+            <TopRatedCard
+              img={img}
+              location={location}
+              title={title}
+              price={price}
+              reviews={reviews}
+            />
+          </div>;
+        })}
       </div>
       <ShowAll title='Show all experiences' />
     </>
