7718e70527a75a12ac70e828494827aace171657
kokiebisu <a01056715@gmail.com>
2020-02-08 16:43:46 -0800

    fixed #80

--- src/components/Stay.js
+++ src/components/Stay.js
@@ -15,81 +15,80 @@ import stay7 from '../../public/img/stay-7.jpg';
 import stay8 from '../../public/img/stay-8.jpg';
 
 export default () => {
+  const stays = [
+    {
+      type: 'normal',
+      img: stay1,
+      location: 'Sol Madrid',
+      title: 'Private Studio in Sol, Madrid',
+      price: 91
+    },
+    {
+      type: 'superhost',
+      img: stay2,
+      location: 'Makawao',
+      title: 'Adorable Garden Gingerbread',
+      price: 257
+    },
+    {
+      type: 'superhost',
+      img: stay3,
+      location: 'Roma Norte',
+      title: 'Radiant Apartment with Terrace in...',
+      price: '104'
+    },
+    {
+      type: 'superhost',
+      img: stay4,
+      location: 'Portici',
+      title: 'Villa San Gennariello',
+      price: 87
+    },
+    {
+      type: 'plus',
+      img: stay5,
+      location: 'Old Town',
+      title: 'Classical Apartment on the Royal...',
+      price: 206
+    },
+    {
+      type: 'normal',
+      img: stay6,
+      location: 'Porto',
+      title: 'PORTA33 | PORTO PENTHOUSE',
+      price: 126
+    },
+    {
+      type: 'superhost',
+      img: stay7,
+      location: 'Krakow',
+      title: 'Amazing and Extremely Central Flat',
+      price: 31
+    },
+    {
+      type: 'superhost',
+      img: stay8,
+      location: 'Tricase',
+      title: 'La Salentina, sea, nature & relax',
+      price: 68
+    }
+  ];
   return (
     <>
       <div className='flex flex-wrap items-start justify-start w-full'>
-        <div className='w-1/2 lg:w-1/3 xl:w-1/4 pb-5'>
-          <StayCard
-            type='normal'
-            img={stay1}
-            location='Sol Madrid'
-            title='Private Studio in Sol, Madrid'
-            price='91'
-          />
-        </div>
-        <div className='w-1/2 lg:w-1/3 xl:w-1/4 pb-5'>
-          <StayCard
-            type='superhost'
-            img={stay2}
-            location='Makawao'
-            title='Adorable Garden Gingerbread...'
-            price='257'
-          />
-        </div>
-        <div className='w-1/2 lg:w-1/3 xl:w-1/4 pb-5'>
-          <StayCard
-            type='superhost'
-            img={stay3}
-            location='Roma Norte'
-            title='Radiant Apartment with Terrace in...'
-            price='104'
-          />
-        </div>
-        <div className='w-1/2 lg:w-1/3 xl:w-1/4 pb-5'>
-          <StayCard
-            type='superhost'
-            img={stay4}
-            location='Portici'
-            title='Villa San Gennariello'
-            price='87'
-          />
-        </div>
-        <div className='w-1/2 lg:w-1/3 xl:w-1/4 pb-5'>
-          <StayCard
-            type='plus'
-            img={stay5}
-            location='Old Town'
-            title='Classical Apartment on the Royal...'
-            price='206'
-          />
-        </div>
-        <div className='w-1/2 lg:w-1/3 xl:w-1/4 pb-5'>
-          <StayCard
-            type='normal'
-            img={stay6}
-            location='Porto'
-            title='PORTA33 | PORTO PENTHOUSE...'
-            price='126'
-          />
-        </div>
-        <div className='w-1/2 lg:w-1/3 xl:w-1/4 pb-5'>
-          <StayCard
-            type='superhost'
-            img={stay7}
-            location='Krakow'
-            title='Amazing and Extremely Central Flat'
-            price='31'
-          />
-        </div>
-        <div className='w-1/2 lg:w-1/3 xl:w-1/4 pb-5'>
-          <StayCard
-            type='superhost'
-            img={stay8}
-            location='Tricase'
-            title='La Salentina, sea, nature & relax'
-            price='68'
-          />
-        </div>
+        {stays.map(({ type, img, location, title, price }) => {
+          return (
+            <div className='w-1/2 lg:w-1/3 xl:w-1/4 pb-5'>
+              <StayCard
+                type={type}
+                img={img}
+                location={location}
+                title={title}
+                price={price}
+              />
+            </div>
+          );
+        })}
       </div>
       <ShowAll title='Show(2000+)' />
     </>
