01ee7d03c5fffe2c6c94ffb801d3e9cfb134ce0b
kokoaj <a01056715@gmail.com>
2020-02-13 13:18:11 -0800

    fixed #135

--- frontend/src/components/containers/Neighborhood.js
+++ frontend/src/components/containers/Neighborhood.js
@@ -21,8 +21,22 @@ export default () => {
         </p>
         <button
           style={{ fontFamily: 'airbnb-medium' }}
-          className='mt-3 mb-5 hover:border-green-850 border-b text-green-850 border-transparent'>
-          Read more about the neighbourhood
+          className='mt-3 mb-5 text-green-850'>
+          <div className='flex items-center justify-start'>
+            <p
+              className='text-green-850 hover:border-green-850 border-b text-green-850 border-transparent'
+              style={{ fontFamily: 'airbnb-medium' }}>
+              Read about the neighborhood
+            </p>
+            <div className='ml-3'>
+              <svg
+                style={{ fill: '#008489' }}
+                viewBox='0 0 18 18'
+                className='h-2 w-2'>
+                <path d='m16.29 4.3a1 1 0 1 1 1.41 1.42l-8 8a1 1 0 0 1 -1.41 0l-8-8a1 1 0 1 1 1.41-1.42l7.29 7.29z' />
+              </svg>
+            </div>
+          </div>
         </button>
         <div className='bg-blue-500 h-64'></div>
         <p style={{ fontFamily: 'airbnb-book' }} className='text-gray-750 mt-2'>
