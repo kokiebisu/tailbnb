d44af5e3a6387b61d8b4b1a68bede281de0a6d86
kokoaj <a01056715@gmail.com>
2020-02-24 11:47:22 -0800

    finished explore type

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bea28c4630126dccc95e";
+/******/ 	var hotCurrentHash = "0a478e759434e886193e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ frontend/src/components/containers/Explore.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+
+// Components
+import { ExploreCard } from '../presentational/ExploreCard';
+
+// Images
+// High Resolution
+const explore1 = require('../../../public/img/high/explore1.jpg');
+const explore2 = require('../../../public/img/high/explore2.jpg');
+const explore3 = require('../../../public/img/high/explore3.jpg');
+
+interface Explore {
+  img: string;
+  title: string;
+}
+
+export const Explore: React.FC<{}> = () => {
+  const explores: Explore[] = [
+    {
+      img: explore1,
+      title: 'Stays'
+    },
+    {
+      img: explore2,
+      title: 'Experiences'
+    },
+    {
+      img: explore3,
+      title: 'Adventures'
+    }
+  ];
+  return (
+    <div className='overflow-y-hidden'>
+      <div className='w-full h-full overflow-y-hidden'>
+        <div className='h-full scroller'>
+          <div className='scrollable sm:inset-x-0 flex items-center justify-start py-2 rounded-xl w-80 md:w-full'>
+            {explores.map(({ img, title }, index) => {
+              return (
+                <div className='w-32 mr-4 sm:w-1/3 cursor-pointer'>
+                  <ExploreCard key={index} img={img} title={title} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
