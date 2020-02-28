a19e9dac4143ac381e882d55f84b78867770e6b3
Ken <kenichikona@gmail.com>
2020-02-27 20:31:03 -0800

    finished plus test type

--- frontend/__tests__/containers/plus.test.tsx
+++ frontend/__tests__/containers/plus.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import { shallow } from 'enzyme';
 
 // Component
-import Plus from '../../src/components/containers/Plus';
+import { Plus } from '../../src/components/containers/Plus';
 
 describe('Plus', () => {
   it('should render correctly in "debug" mode', () => {
