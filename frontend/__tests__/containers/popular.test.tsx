f667c66fc8db507607a06879fd54985162f034fb
Ken <kenichikona@gmail.com>
2020-02-27 20:29:02 -0800

    finished popular test type

--- frontend/__tests__/containers/popular.test.tsx
+++ frontend/__tests__/containers/popular.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import { shallow } from 'enzyme';
 
 // Component
-import Popular from '../../src/components/containers/Popular';
+import { Popular } from '../../src/components/containers/Popular';
 
 describe('Popular', () => {
   it('should render correctly in "debug" mode', () => {
