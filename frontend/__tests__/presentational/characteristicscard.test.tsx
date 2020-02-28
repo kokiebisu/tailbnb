00fb21e9b6e389a37a91b94386225a4c99d20558
Ken <kenichikona@gmail.com>
2020-02-27 19:23:13 -0800

    finished characteristic card test type

--- frontend/__tests__/presentational/characteristicscard.test.tsx
+++ frontend/__tests__/presentational/characteristicscard.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import { shallow } from 'enzyme';
 
 // Components
-import CharacteristicCard from '../../src/components/presentational/CharacteristicCard';
+import { CharacteristicCard } from '../../src/components/presentational/CharacteristicCard';
 
 describe('Characteristics Card', () => {
   it('should render correctly in "debug" mode', () => {
