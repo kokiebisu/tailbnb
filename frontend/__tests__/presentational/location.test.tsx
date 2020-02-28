9f813764bf2d7c0a3ed4c430f6211b95c75c77c6
Ken <kenichikona@gmail.com>
2020-02-27 19:39:24 -0800

    finished location test type

---
+++ frontend/__tests__/presentational/location.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+// Component
+import { Location } from '../../src/components/presentational/Location';
+
+describe('Location', () => {
+  it('should render correct in "debug" mode', () => {
+    const info = {
+      location: 'testlocation',
+      price: 10
+    };
+    const component = shallow(<Location {...info} />);
+    expect(component).toMatchSnapshot();
+  });
+});
