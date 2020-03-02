d951bd63211f687e802983b6d302f906f1d7f348
Ken <kenichikona@gmail.com>
2020-03-01 17:58:53 -0800

    finished adventure component

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1d4c9a1cbcc12e3baafc";
+/******/ 	var hotCurrentHash = "7a36d81ed75d4dda5ce5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
