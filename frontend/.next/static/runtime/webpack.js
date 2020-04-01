1e3cf7f7d79dab84e21815b560c8c45dfa208be5
Ken <kenichikona@gmail.com>
2020-03-31 17:04:12 -0700

    fixed setcurrency for experiencesheader

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "75d75a1561c92897166d";
+/******/ 	var hotCurrentHash = "29875ec92cddcd1a4bd7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
