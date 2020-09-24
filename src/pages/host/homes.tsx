41b6ea463ac84966227f0ff89245c007d011e6c5
Kenichi Okiebisu <a01056715@gmail.com>
2020-09-23 23:56:02 -0700

    host home page banner

--- src/pages/host/homes.tsx
+++ src/pages/host/homes.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BottomArrow, NoNameLogo } from 'assets/svg';
+import { BottomArrow, NoNameLogo, QuestionMark } from 'assets/svg';
 import Link from 'next/link';
 import styles from 'styles/host/homes.module.scss';
 
@@ -63,6 +63,75 @@ const HostHome = () => {
                 </Link>
               </div>
             ))}
+          </div>
+        </div>
+      </div>
+      <div className={styles['content']}>
+        <div className={styles['content__inner']}>
+          <img
+            src='/img/host-home.jpg'
+            className={styles['content__background']}
+          />
+          <div className={styles['content__title']}>
+            <h3>Earn money as an Airbnb host</h3>
+          </div>
+        </div>
+        <div className={styles['banner-card']}>
+          <div className={styles['banner-card__inner']}>
+            <div className={styles['banner-card__title']}>
+              <h3>Earn money as an Airbnb host</h3>
+            </div>
+            <div className={styles['banner-card__subtitle']}>
+              <p>Find out what you could earn</p>
+            </div>
+            <div className={styles['banner-card__input--location']}>
+              <input type='text' placeholder='Location' />
+            </div>
+            <div className={styles['banner-card__input--others']}>
+              <div className={styles['banner-card__input--place']}>
+                <select id='guests'>
+                  <option value='1'>Entire place</option>
+                  <option value='2'>Private room</option>
+                  <option value='3'>Shared room</option>
+                </select>
+                <div>
+                  <svg viewBox='0 0 20 20'>
+                    <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+                  </svg>
+                </div>
+              </div>
+              <div className={styles['banner-card__input--guest']}>
+                <select id='guests'>
+                  <option value='1'>1 guest</option>
+                  <option value='2'>2 guests</option>
+                  <option value='3'>3 guests</option>
+                  <option value='4'>4 guests</option>
+                  <option value='5'>5 guests</option>
+                  <option value='6'>6 guests</option>
+                </select>
+                <div>
+                  <svg viewBox='0 0 20 20'>
+                    <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div className={styles['banner-card__price']}>
+              <div>
+                <h3>$1,162</h3>
+              </div>
+              <div>
+                <p>per month</p>
+              </div>
+              <div>
+                <div className={styles['banner-card__icon--question']}>
+                  <QuestionMark fill='#484848' />
+                </div>
+              </div>
+            </div>
+            <button className={styles['banner-card__button']}>
+              <p>Get started</p>
+            </button>
           </div>
         </div>
       </div>
